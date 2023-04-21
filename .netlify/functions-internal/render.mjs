02cc751b9004e2f3575b801153f722e398a59dc4
Chadwick Macmillan <chadwickmacmillan@gmail.com>
2023-04-21 17:35:14 -0400

    Revert "Revert "build""
    This reverts commit 37cfe6f2f6bbe4da09c99f99f4ef1f7eb0a12e98.

--- .netlify/functions-internal/render.mjs
+++ .netlify/functions-internal/render.mjs
@@ -6,7 +6,7 @@ export const handler = init({
 	assets: new Set(["fonts/clearlight.woff2","fonts/ortica-light.woff2","fonts/source-serif-pro.woff2"]),
 	mimeTypes: {".woff2":"font/woff2"},
 	_: {
-		client: {"start":{"file":"_app/immutable/entry/start.9054f0b0.js","imports":["_app/immutable/entry/start.9054f0b0.js","_app/immutable/chunks/index.e0c1dd86.js","_app/immutable/chunks/singletons.89b4eb60.js","_app/immutable/chunks/index.cd539fef.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.d2aa3f11.js","imports":["_app/immutable/entry/app.d2aa3f11.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/index.e0c1dd86.js"],"stylesheets":[],"fonts":[]}},
+		client: {"start":{"file":"_app/immutable/entry/start.9f69ed52.js","imports":["_app/immutable/entry/start.9f69ed52.js","_app/immutable/chunks/index.e0c1dd86.js","_app/immutable/chunks/singletons.1af9c7f9.js","_app/immutable/chunks/index.cd539fef.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.e331a077.js","imports":["_app/immutable/entry/app.e331a077.js","_app/immutable/chunks/preload-helper.41c905a7.js","_app/immutable/chunks/index.e0c1dd86.js"],"stylesheets":[],"fonts":[]}},
 		nodes: [
 			() => import('../server/nodes/0.js'),
 			() => import('../server/nodes/1.js'),
